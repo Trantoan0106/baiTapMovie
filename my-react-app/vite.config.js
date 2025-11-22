01a7b4b2c9accc8be6d59cb44242c271aa79681b
Trantoan0106 <trankhanhtoan.290400@gmail.com>
2025-11-22 21:27:06 +0700

    Set base for GitHub Pages

--- my-react-app/vite.config.js
+++ my-react-app/vite.config.js
@@ -1,42 +1,34 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig , loadEnv } from "vite"
-
-
+import { defineConfig, loadEnv } from "vite"
 
 export default ({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '') 
+  const env = loadEnv(mode, process.cwd(), "")
 
+  // https://vite.dev/config/
+  return defineConfig({
+    // 👇 QUAN TRỌNG: tên repo trên GitHub Pages
+    base: "/baiTapMovie/",
 
-// https://vite.dev/config/
-return defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-   proxy: {
-    '/cyber': {
-    target: 'https://movienew.cybersoft.edu.vn',
-    changeOrigin: true,
-    secure:true,
-rewrite: p => p.replace(/^\/cyber/, ''),
-headers: {
-   TokenCybersoft: env.VITE_CYBERSOFT_TOKEN,
-}
-
-    }
-
-
-
-
-   }
-
-
-
-  }
-})
+    server: {
+      proxy: {
+        "/cyber": {
+          target: "https://movienew.cybersoft.edu.vn",
+          changeOrigin: true,
+          secure: true,
+          rewrite: (p) => p.replace(/^\/cyber/, ""),
+          headers: {
+            TokenCybersoft: env.VITE_CYBERSOFT_TOKEN,
+          },
+        },
+      },
+    },
+  })
 }
